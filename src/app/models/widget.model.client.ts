a17cd1f71b6d018acb594f8d050a0492e0ace287
pascalguerre1 <pascalguerre@hotmail.com>
2018-07-30 10:59:36 -0400

    A9 widget text

--- src/app/models/widget.model.client.ts
+++ src/app/models/widget.model.client.ts
@@ -7,6 +7,9 @@ export class Widget {
 	text?: string;
 	width?: string;
 	url?: string;
+	rows?: number;
+	placeholder?: string;
+	formatted?: boolean;
 
 	constructor(_id, widgetType, pageId) {
 		this._id = _id;
