42f880a04c18611217a1411b520dfaa410adfdfe
pascalguerre1 <pascalguerre@hotmail.com>
2018-08-13 13:08:28 -0400

    A9

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -4,7 +4,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += '@ds0000.mlab.com:29004/dasdasds'; // use yours
+  connectionString += '@ds121262.mlab.com:21262/heroku_cq1239dj'; // use yours
 }
 
 var mongoose = require("mongoose");
